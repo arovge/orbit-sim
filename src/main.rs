adbba4cdff46cd5431e9b4b2ac7a74e8a7fe27f8
Austin Rovge <austinrovge@gmail.com>
2026-02-05 18:58:21 -0600

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod commands;
 mod components;
+mod plugins;
 mod state;
-mod systems;
 
 use bevy::prelude::*;
 use commands::SpawnAsteroidCommand;
-use state::*;
-use systems::{
+use plugins::{
     asteroid_drag::AsteroidDragPlugin, input::InputPlugin, physics::PhysicsPlugin,
     planet_editor::PlanetEditorPlugin, ui::UiPlugin,
 };
+use state::*;
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
